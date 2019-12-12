c40b9ced1c694d694151abbca45769c60fa57f42
Gero <geronimo@devsar.com>
2019-12-12 14:28:12 -0300

    Router working with links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Nav from './components/nav';
-import Home from './views/home';
-import Shop from './views/shop';
-import About from './views/about';
-import Versus from './views/versus';
+import Nav from "./components/nav";
+import Home from "./views/home";
+import Shop from "./views/shop";
+import About from "./views/about";
+import Versus from "./views/versus";
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
       <Router>
+        <Nav />
         <Switch>
-          <Route path="/" exact component={Home}/>  
-          <Route path="/shop" exact component={Shop}/>  
-          <Route path="/about" exact component={About}/>  
-          <Route path="/versus" exact component={Versus}/>  
+          <Route path="/" exact component={Home} />
+          <Route path="/shop" exact component={Shop} />
+          <Route path="/about" exact component={About} />
+          <Route path="/versus" exact component={Versus} />
         </Switch>
       </Router>
     </div>
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Nav = () => {
     return(
         <nav>
-            <h1>Video Games Shop</h1>
+            <div className="page-title">
+                <h1>Video Games Shop</h1>
+            </div>
+
+            <div className="menu">
+                <ul>
+                    <Link to="/">
+                        <li>Home</li>
+                    </Link>
+                    <Link to="/about">
+                        <li>About</li>  
+                    </Link>
+                    <Link to="/shop">
+                        <li>Shop</li>
+                    </Link>
+                    <Link to="/versus">
+                        <li>Versus</li>
+                    </Link>
+                </ul>
+            </div>
         </nav>
     );
 };

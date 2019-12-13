dbc653d2e0ce78041038e7e7b90eb0dcccaaddf5
Gero <geronimo@devsar.com>
2019-12-13 10:19:10 -0300

    Nav styling done

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Link} from 'react-router-dom';
+import '../style/nav.module.css';
+import { styles } from 'ansi-colors';
 
 const Nav = () => {
+
+    const [display, setDisplay] = useState('none');
+    const [selected, setSelected] = useState('none');
+
+    let menuStyle = {
+        display
+    }
+
+    const toggleDisplay = () => {
+        if(display === 'none'){
+            setDisplay('flex');
+        } else if(display === 'flex'){
+            setDisplay('none');
+        }
+    };
+
     return(
         <nav>
-            <div className="page-title">
-                <h1>Video Games Shop</h1>
+            <div className={styles.pageTitle}>
+                <h1 className="title">Video Games Shop</h1>
             </div>
 
             <div className="menu">
-                <ul>
+                <h4 onClick={toggleDisplay}>Menú</h4>
+                <ul style={menuStyle}>
                     <Link to="/">
                         <li>Home</li>
                     </Link>

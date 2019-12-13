681eac0bd5618c15b6ce5db4e4ad87cdb994bb88
Gero <geronimo@devsar.com>
2019-12-13 09:30:56 -0300

    Added footer structure

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>Footer</h1>
+            <a href="https://rapidapi.com/accujazz/api/rawg-video-games-database/details">
+                Data provided by RAWG
+            </a>
+        </footer>
+    );
+};
+
+export default Footer;

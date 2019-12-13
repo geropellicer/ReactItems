c380ea667cbc63cd5fd9a7d532026e7791729958
Gero <geronimo@devsar.com>
2019-12-13 10:23:33 -0300

    Footer styling done

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import '../style/footer.module.css';
 
 const Footer = () => {
     return (
         <footer>
-            <h1>Footer</h1>
+            <h5>Video Games Shop -&nbsp;</h5>
             <a href="https://rapidapi.com/accujazz/api/rawg-video-games-database/details">
-                Data provided by RAWG
+                Data kindly provided by RAWG
             </a>
+            <h5>&nbsp;- RAWG</h5>
         </footer>
     );
 };
